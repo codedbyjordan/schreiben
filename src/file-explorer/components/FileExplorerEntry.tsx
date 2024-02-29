4d36bbd44ad927a428c3d8c749043ebf52376c85
Jordan Baron <62375480+codedbyjordan@users.noreply.github.com>
2024-02-29 11:54:27 -0500

    🛠️ tweak: File explorer tweaks (#6)
    * collapsible folders
    
    * add folder chevron icon
    
    * rm unused import

--- src/file-explorer/components/FileExplorerEntry.tsx
+++ src/file-explorer/components/FileExplorerEntry.tsx
@@ -1,11 +1,13 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import { FileEntry, readTextFile } from "@tauri-apps/api/fs";
+import { useState } from "react";
 import { useActiveFileStore } from "../hooks/useActiveFileStore";
 
 type FolderProps = FileEntry;
 
 export function FileExplorerEntry({ name, path, children }: FolderProps) {
+  const [isOpen, setIsOpen] = useState(false);
   const hasChildFiles = children && children.length > 0;
-
   const activeFileStore = useActiveFileStore();
 
   const setActiveFile = async ({ name, path }: Omit<FileEntry, "children">) => {
@@ -17,25 +19,40 @@ export function FileExplorerEntry({ name, path, children }: FolderProps) {
     });
   };
 
+  const isFolder = !!children;
+
+  const toggleOrSetActiveFile = ({
+    name,
+    path,
+  }: Omit<FileEntry, "children">) => {
+    if (isFolder) setIsOpen(!isOpen);
+    else setActiveFile({ name, path });
+  };
+
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col items-start gap-1">
       <button
-        className="rounded-sm transition-colors hover:bg-neutral-600"
-        onClick={() => !children && setActiveFile({ name, path })}
+        className="flex items-center gap-1 rounded-sm transition-colors hover:bg-neutral-600"
+        onClick={() => toggleOrSetActiveFile({ name, path })}
       >
-        {name}
+        {isFolder && (
+          <ChevronRightIcon className={`h-4 w-4 ${isOpen && "rotate-90"}`} />
+        )}
+        <span>{name}</span>
       </button>
-      <div className="ml-4">
-        {hasChildFiles &&
-          children?.map((child) => (
-            <FileExplorerEntry
-              key={child.path}
-              name={child.name}
-              path={child.path}
-              children={child.children}
-            />
-          ))}
-      </div>
+      {isOpen && (
+        <div className="ml-4">
+          {hasChildFiles &&
+            children?.map((child) => (
+              <FileExplorerEntry
+                key={child.path}
+                name={child.name}
+                path={child.path}
+                children={child.children}
+              />
+            ))}
+        </div>
+      )}
     </div>
   );
 }
