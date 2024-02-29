a9fcd46af7bd12f480bc2dea995e426128fa3f44
Jordan Baron <62375480+codedbyjordan@users.noreply.github.com>
2024-02-29 00:25:53 -0500

    Custom Titlebar (#4)
    * permissions
    
    * add heroicons
    
    * add titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Editor } from "@/editor";
 import { FileExplorer } from "./file-explorer";
+import { Titlebar } from "./components/Titlebar";
 
 export function App() {
   return (
-    <div className="flex h-screen w-full justify-center bg-neutral-900 font-sans text-white">
-      <FileExplorer />
-      <Editor />
+    <div className="flex h-screen w-full flex-col bg-neutral-900 font-sans text-white">
+      <Titlebar />
+      <div className="flex h-full">
+        <FileExplorer />
+        <Editor />
+      </div>
     </div>
   );
 }
